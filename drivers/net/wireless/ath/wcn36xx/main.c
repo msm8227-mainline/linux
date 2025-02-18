a12f280dc9f7be363c37d002b9160fb9ff349088
wctrl <166127285+wctrl@users.noreply.github.com>
2025-02-18 18:22:55 +0200

    remove wifi 5ghz panic

--- drivers/net/wireless/ath/wcn36xx/main.c
+++ drivers/net/wireless/ath/wcn36xx/main.c
@@ -1302,7 +1302,6 @@ static int wcn36xx_get_survey(struct ieee80211_hw *hw, int idx,
 	sband = wcn->hw->wiphy->bands[NL80211_BAND_2GHZ];
 	band_idx = idx;
 	if (band_idx >= sband->n_channels) {
-		panic("no 5ghz supported for fame");
 		band_idx -= sband->n_channels;
 		sband = wcn->hw->wiphy->bands[NL80211_BAND_5GHZ];
 	}
